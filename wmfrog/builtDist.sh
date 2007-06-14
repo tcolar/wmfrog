a28050cb957afc6b8502699dc618e1337a6d8425
tcolar <devnull@localhost>
2007-06-14 08:41:04 -0700

    [svn r8]

--- wmfrog/builtDist.sh
+++ wmfrog/builtDist.sh
@@ -3,7 +3,13 @@
 #clean
 rm Src/*.o Src/wmfrog
 
-#source tgz
+#source tgz in dist
 tar czvf dist/wmfrog.tgz Src CHANGES COPYING HINTS INSTALL
+
+#build and copy to dist
+cd Src
+make
+cp wmfrog dist
+cp weather.pl dist
 
 #deb  TBD
